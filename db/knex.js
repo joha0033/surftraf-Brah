b15dfa0bb3c68e6f3b668e4d57e5cb29c58c9c87
Jason Clifton <j.clifton1473@gmail.com>
2017-06-16 15:05:31 -0600

    added production and Heroku.

--- db/knex.js
+++ db/knex.js
@@ -1,4 +1,4 @@
-const environment = process.env.NODE_URL || 'development'
+const environment = process.env.NODE_ENV || 'development'
 const config = require('../knexfile')
 const configEnvironment = config[environment];
 const knex = require('knex');
--- knexfile.js
+++ knexfile.js
@@ -5,5 +5,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgresql://localhost/surftraf'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 };
